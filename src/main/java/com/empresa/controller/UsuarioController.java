7c3a3e3b746b3104242f83c08933e877b25143d9
kumatsu <kumatsu.kun@gmail.com>
2021-09-22 00:02:39 -0500

    backend preparado 1.1

--- src/main/java/com/empresa/controller/UsuarioController.java
+++ src/main/java/com/empresa/controller/UsuarioController.java
@@ -4,16 +4,17 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.empresa.entity.Usuario;
 import com.empresa.service.UsuarioService;
 
 @RestController
 @RequestMapping("rest/usuario")
+@CrossOrigin(origins = "http://localhost:4200")
 public class UsuarioController {
 	
 	@Autowired
